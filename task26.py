de83a48aeac385e4e001f7056e81942be5498c88
Anya Denisova <bilberian@mail.ru>
2023-02-12 15:28:17 +0300

    HW5

---
+++ task26.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая на вход принимает два числа A и B, 
+# и возводит число А в целую степень B с помощью рекурсии.
+# A = 3; B = 5 -> 243 (3⁵)
+# A = 2; B = 3 -> 8
+
+a = int(input('insert a: '))
+b = int(input('insert b: '))
+
+def recf(a, b):
+    if(b == 0):
+        return 1; 
+    if(b == 1):
+        return a; 
+    return a * recf(a, b - 1)
+
+print(recf(a, b))
